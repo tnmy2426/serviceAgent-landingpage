3d106bc4c6b2571119b1885a1d2faa567aa1f615
tnmy2426 <al.nahdi.tnmy@gmail.com>
2022-02-19 21:39:30 +0600

    Header section complete. Night mode button added. Navbar and mobile nav button completed also. Responsive behavior okay.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@ module.exports = {
    darkMode: "class",
    content: ["./public/index.html"],
    theme: {
-      extend: {},
+      extend: {
+         backgroundImage: {
+            header_bg: "url('./img/bgHeader.png')",
+         },
+         colors: {
+            main_clr: "#f8f8f8",
+            secondary_clr: "#f95157",
+            title_clr: "#1b2f4d",
+         },
+      },
+
+      container: {
+         center: true,
+         padding: "3rem",
+      },
    },
    plugins: [],
 };
